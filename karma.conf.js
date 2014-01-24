855eea26049f5115ea8fe5a52bc4cb8cd4344199
Ben Drucker <bvdrucker@gmail.com>
2014-01-23 21:48:18 -0500

    Test setup

--- karma.conf.js
+++ karma.conf.js
@@ -4,26 +4,22 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     files: [
-      'mock/socket-io.js',
+      'mock/sockjs-client.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'socket.js',
       '*.spec.js'
     ],
 
-    preprocessors: {
-      'socket.js': ['coverage']
-    },
-
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress'],
 
     port: 9876,
     colors: true,
 
     logLevel: config.LOG_INFO,
 
-    browsers: ['Chrome'],
-    frameworks: ['jasmine'],
+    browsers: ['PhantomJS'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
     captureTimeout: 60000,
 
